6e15df9ef98e972068a451706a1e682e806d6849
sosb <bence.sos92@gmail.com>
2018-02-05 20:12:04 +0100

    first solution

--- src/main/java/order/Order.java
+++ src/main/java/order/Order.java
@@ -1,7 +1,32 @@
 package order;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Order {
-  public static String order(String words) {
-    return null;
-  }
+
+	private static final String EMPTY_STRING = "";
+
+	public static String order(String words) {
+		if (words == null || isWhite(words)) {
+			return EMPTY_STRING;
+		}
+		String[] chars = words.split(" ");
+		Arrays.sort(chars, Comparator.comparingInt(o -> o.charAt(getNumberIndex(o))));
+		return String.join(" ", chars);
+	}
+
+	private static int getNumberIndex(String word) {
+		int index = 0;
+		for (int i = 0; i < word.length(); i++) {
+			if (Character.isDigit(word.charAt(i))) {
+				index = i;
+			}
+		}
+		return index;
+	}
+
+	private static boolean isWhite(String word) {
+		return word.trim().isEmpty();
+	}
 }
--- src/test/java/order/OrderTest.java
+++ src/test/java/order/OrderTest.java
@@ -1,34 +1,37 @@
 package order;
 
-import static org.junit.Assert.*;
-
-import order.Order;
 import org.junit.Test;
-import static org.hamcrest.CoreMatchers.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class OrderTest {
-  @Test
-  public void test1() {
-    assertThat(Order.order("is2 Thi1s T4est 3a"), equalTo("Thi1s is2 3a T4est"));
-  }
-  
-  @Test
-  public void test2() {
-    assertThat(Order.order("4of Fo1r pe6ople g3ood th5e the2"), equalTo("Fo1r the2 g3ood 4of th5e pe6ople"));
-  }
-  
-  @Test
-  public void test3() {
-    assertThat(Order.order("d4o dru7nken sh2all w5ith s8ailor wha1t 3we a6"), equalTo("wha1t sh2all 3we d4o w5ith a6 dru7nken s8ailor"));
-  }
-  
-  @Test
-  public void test4() {
-    assertThat("Empty input should return empty string", Order.order(""), equalTo(""));
+	@Test
+	public void test1() {
+		assertEquals("Thi1s is2 3a T4est", Order.order("is2 Thi1s T4est 3a"));
 	}
-  
-  @Test
-  public void test5() {
-    assertThat(Order.order("3 6 4 2 8 7 5 1 9"), equalTo("1 2 3 4 5 6 7 8 9"));
-  }
+
+	@Test
+	public void test2() {
+		assertEquals("Fo1r the2 g3ood 4of th5e pe6ople", Order.order("4of Fo1r pe6ople g3ood th5e the2"));
+	}
+
+	@Test
+	public void test3() {
+		assertEquals("wha1t sh2all 3we d4o w5ith a6 dru7nken s8ailor", Order.order("d4o dru7nken sh2all w5ith s8ailor wha1t 3we a6"));
+	}
+
+	@Test
+	public void test4() {
+		assertEquals("", Order.order(""));
+	}
+
+	@Test
+	public void test5() {
+		assertEquals("1 2 3 4 5 6 7 8 9", Order.order("3 6 4 2 8 7 5 1 9"));
+	}
+
+	@Test
+	public void test6() {
+		assertEquals("is2 3a T4est This", Order.order("is2 This T4est 3a"));
+	}
 }
